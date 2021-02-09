f8521a198bdeeb2ccef78353c1ee4898f167ba49
sam-malanchuk <malanchuksam@gmail.com>
2021-02-09 15:01:55 -0500

    moved the hero details into it's own route and linked all the heroes through routes with ID's rather than a selectedHero variable

--- heroes/src/app/hero-detail/hero-detail.component.ts
+++ heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService } from '../hero.service';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,12 +11,21 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  @Input() hero: Hero;
-
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 }
