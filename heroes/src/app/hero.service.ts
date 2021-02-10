c30397052091b0215de34fd2a16cfe077cab3807
sam-malanchuk <malanchuksam@gmail.com>
2021-02-10 16:57:24 -0500

    injecting the HTTP client to make requests from the hero service module

--- heroes/src/app/hero.service.ts
+++ heroes/src/app/hero.service.ts
@@ -1,17 +1,30 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    // sending a message to confirm heroes have been fetched
-    this.messageService.add('HeroService: fetched heroes')
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
@@ -19,6 +32,4 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
-  
-  constructor(private messageService: MessageService) { }
 }
